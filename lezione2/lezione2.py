8037faf0da5a40deaf5d85fb68af17860832df91
Artemis173 <edyenwere@gmail.com>
2024-04-17 14:04:59 +0200

    Aggiunto file lezione2.py

---
+++ lezione2/lezione2.py
@@ -0,0 +1,10 @@
+#Edwin
+print ("Hello World")
+
+# Use a variable to represent a person's name, 
+# and print a message to that person. 
+# Your message should be simple , such as, 
+# "Hello Eric, would youl like to learn some python today?"
+
+name: str = "Eric"
+message: str = f"Ciao {name} ti va di imparre un po di Python insieme?"

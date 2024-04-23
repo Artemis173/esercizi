f3c2c12624742e38ccb781d234cd951db7786713
Artemis173 <edyenwere@gmail.com>
2024-04-23 13:47:41 +0200

    Update

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -1,3 +1,4 @@
+
 # Think of at least three kinds of your favorite pizza. 
 # Store these pizza names in a list, and then use a for loop to print the name of each pizza.
 # - Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza.
@@ -22,6 +23,7 @@ for pizza in Pizzas:
     else:
         print("Non posso mangiare la Diavola")
 print("\nMi piaciono davvero poche pizze!")
+
 
 # Think of at least three different animals that have a common characteristic. 
 # Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
@@ -78,5 +80,129 @@ odd_number = list(range(1, 20, 2))
 for number in odd_number:
     print(number)
 
+# Make a list of the multiple of the nuumber 3, from 3 to 30.
+# Use a for loop to print the number in your list
 
-#
+x: int = 0
+num: list = [number for number in range(3,31) if number%3==0]
+for i in num:
+    print(f"{i}",end=" ")
+print("")
+
+# A number raised to the third power is called a cube. 
+# For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that is,
+# the cube of each integer from 1 through 10), and use a for loop to print out the value of each cube.
+
+print([num ** 3 for num in range (1, 11)])
+
+# Use a list comprehension to generate a list of the first 10 cubes.
+
+num: list = ([num**3 for num in range (1,11)])
+print(num)
+
+# Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+# - Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+# - Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
+# - Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+
+Personaggi: list = ["Gilgamesh", "Enkidu", "Ushiwakamaru", "Gorgone", "Semiranide", "Leonida", "Saber", "Archer"]
+x = slice(3)
+y = slice(3, 5)
+z = slice(5, 8)
+print(Personaggi[x])
+print(Personaggi[y])
+print(Personaggi[z])
+
+# Write a series of conditional tests. Print a statement
+# describing each test and your prediction for the results of each test. Your code
+# should look something like this:
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# Create at least 10 tests. Have at least 5 tests evaluate to True and another
+# 5 tests evaluate to False.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru') 
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru') 
+print("\nIs car == 'maserati'? I predict False.")
+print(car == 'maserati')
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru') 
+print("\nIs car == 'toyota'? I predict False.")
+print(car == 'toyota')
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru') 
+print("\nIs car == 'ferrari'? I predict False.")
+print(car == 'ferrari')
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru') 
+print("\nIs car == 'opel'? I predict False.")
+print(car == 'opel')
+
+# You don’t have to limit the number of tests you cre-
+# ate to 10. If you want to try more comparisons, write more tests and add them
+
+# Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+# • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+# • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+alien_color = "green"
+green = 5
+
+if alien_color == "green":
+    print("Il giocatore ha totalizato 5 punti!")
+
+elif alien_color == "yellow":
+    print(" Il giocatore ha totalizzato 10 punti!")
+
+else:
+    print("Il giocatore ha totalizzato 15 punti!")
+
+alien_color = "yellow"
+yellow = 10
+
+if alien_color == "green":
+    print("Il giocatore ha totalizato 5 punti!")
+
+elif alien_color == "yellow":
+    print(" Il giocatore ha totalizzato 10 punti!")
+
+else:
+    print("Il giocatore ha totalizzato 15 punti!")
+
+
+# -------
+
+age: int = 21
+
+if age < 2:
+    print("Baby\n")
+elif age < 4:
+    print("Toddler\n")
+elif age < 13:
+    print("Kid\n")
+elif age < 20:
+    print("Teenager\n")
+elif age < 65:
+    print("Adult\n")
+else:
+    print("Elder")
+
+# -------  
